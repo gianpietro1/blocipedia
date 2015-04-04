d5535647c1312a727ac752d209f420eeea9c8b51
Gianpietro Lavado <gianpietro1@gmail.com>
2015-04-04 08:32:50 -0500

    minor fixes

--- spec/features/user-authentication_spec.rb
+++ spec/features/user-authentication_spec.rb
@@ -26,7 +26,7 @@ feature 'User signs in' do
     click_link 'Sign In'
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
-    click_button 'Log in'
+    click_button 'Sign in'
     expect(page).to have_content(' Signed in successfully.')
   end
 
