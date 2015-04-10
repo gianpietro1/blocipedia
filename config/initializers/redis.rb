9317beeeabf644d2c551a4da9bbe871404c7e736
Gianpietro Lavado <gianpietro1@gmail.com>
2015-04-10 18:03:14 -0500

    redis to production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.development?
+ $redis = Redis.new(:host => 'localhost', :port => 6379)
+elsif Rails.env.production?
+ $redis = Redis.new(:url => ENV['REDISTOGO_URL'])
+end
